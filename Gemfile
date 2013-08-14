57fe85b7c2f8e61efcfdd5a23cc11573e6d09048
Ben Gibbs <ben@bobop.co.uk>
2013-08-14 19:46:13 +0100

    added layout and index views

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 gem 'sinatra', "~> 1.4.3"
 gem 'thin'
 gem 'foreman'
+gem 'haml'
--- web.rb
+++ web.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+set :haml, :format => :html5
+
+configure do
+  set :public_folder, Proc.new { File.join(root, "static") }
+end
 
 get '/' do
-  "Hello, world"
+  # "Hello, world"
+  haml :index
 end
